dd2229341384aa1b30ac11cbdd87e37cea64731d
Elmehdi <eaitbrahim@gmail.com>
2020-06-18 16:50:09 +0100

    Add additional test for chunk an array and implement 3 solutions for it

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,37 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //S1
+  //   const chunkedArray = [];
+  //   let chunkArr = [];
+  //   for (let i of array) {
+  //     chunkArr.push(i);
+  //     if (chunkArr.length >= size) {
+  //       chunkedArray.push(chunkArr);
+  //       chunkArr = [];
+  //     }
+  //   }
+  //   if (chunkArr.length > 0) chunkedArray.push(chunkArr);
+  //   console.log(chunkedArray);
+  //   return chunkedArray;
+
+  //S2
+  const chunked = [];
+  //   for (let element of array) {
+  //     const last = chunked[chunked.length - 1];
+  //     if (!last || last.length === size) {
+  //       chunked.push([element]);
+  //     } else {
+  //       last.push(element);
+  //     }
+  //   }
+
+  //S3
+  for (let i = 0; i < array.length; i = i + size) {
+    chunked.push(array.slice(i, i + size));
+  }
+  return chunked;
+}
 
 module.exports = chunk;
